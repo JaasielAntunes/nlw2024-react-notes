7de1260287fe44e9dade9d1fcb24c1a6a94f7194
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-02-07 11:51:50 -0300

    Estilização dos cards

--- src/app.tsx
+++ src/app.tsx
@@ -16,8 +16,20 @@ export function App() {
       <div className="h-px bg-slate-700" />
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
-        <div className="rounded-md bg-slate-700">
+        <div className="rounded-md bg-slate-700 p-5 space-y-3">
+          <span className="text-sm font-medium text-slate-200">Adicionar nota</span>
+          <p className="text-sm leading-4 text-slate-400">
+            Grave uma nota em áudio que será convertida para texto de forma automática.
+          </p>
+        </div>
 
+        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
+          <span className="text-sm font-medium text-slate-300">há 2 dias</span>
+          <p className="text-sm leading-4 text-slate-400">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis, ipsa omnis reprehenderit facilis totam atque! Hic corporis temporibus.
+          </p>
+
+          <div className="absolute bottom-0 right-0 left-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
         </div>
       </div>
     </div>
