459ffcf44aa901ce171e6dd5fcdd177b7a441f74
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-02-07 11:22:46 -0300

    Projeto criado e configurado

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import logo from './assets/logo-nlw-expert.svg';
+
+export function App() {
+  return (
+    <h1 className="text-3xl font-extrabold underline">
+      Hello world!
+    </h1>
+  )
+}
