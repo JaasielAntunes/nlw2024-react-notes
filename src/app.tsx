d8907903093c6fa5a7bcebf6cdc5d5e21c61958c
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-02-07 15:47:41 -0300

    Criar nova nota

--- src/app.tsx
+++ src/app.tsx
@@ -2,11 +2,6 @@ import logo from './assets/logo-nlw-expert.svg';
 import { NewNoteCard } from './components/new-note-card';
 import { NoteCard } from './components/note-card';
 
-const note = {
-  date: new Date(),
-  content: 'Testando...',
-}
-
 export function App() {
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
@@ -24,7 +19,12 @@ export function App() {
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
-        <NoteCard note={note} />
+        <NoteCard
+          note={{
+            date: new Date(),
+            content: "teste...",
+          }}
+        />
       </div>
     </div>
   );
