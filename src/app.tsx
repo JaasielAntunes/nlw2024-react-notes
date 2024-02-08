4b229efc157cd8717e480fba9c739ced19e6eaa1
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-02-08 12:18:22 -0300

    Salvando notas no localstorage, busca de notas e algumas modificações no layout

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import logo from './assets/logo-nlw-expert.svg';
 import { NewNoteCard } from './components/new-note-card';
 import { NoteCard } from './components/note-card';
@@ -10,7 +10,16 @@ interface Note {
 }
 
 export function App() {
-  const [notes, setNotes] = useState<Note[]>([]);
+  const [search, setSearch] = useState("");
+  const [notes, setNotes] = useState<Note[]>(() => {
+    const notesOnStorage = localStorage.getItem("notes");
+
+    if (notesOnStorage) {
+      return JSON.parse(notesOnStorage);
+    }
+
+    return [];
+  });
 
   function onNoteCreated(content: string) {
     const newNote = {
@@ -26,25 +35,42 @@ export function App() {
     localStorage.setItem("notes", JSON.stringify(notesArray));
   }
 
+  function handleSearch(e: ChangeEvent<HTMLInputElement>) {
+    const query = e.target.value;
+    setSearch(query);
+  }
+
+  const filteredNotes =
+    search !== ""
+      ? notes.filter((note) =>
+        note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+      )
+      : notes;
+
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
-      <img src={logo} alt="NLW Expert Notas" />
+      <div className="flex justify-center items-center">
+        <img className="" src={logo} alt="NLW Expert Notas" />
+      </div>
 
-      <form className="w-full">
+      <form className="w-full pt-7">
         <input
           type="text"
           placeholder="Busque em suas notas..."
           className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-state-500"
+          onChange={handleSearch}
         />
       </form>
 
-      <div className="h-px bg-slate-700" />
+      <div className="h-px bg-violet-800" />
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard onNoteCreated={onNoteCreated} />
 
-        {notes.map(note => {
-          return <NoteCard key={note.id} note={note} />
+        {filteredNotes.map((note) => {
+          return (
+            <NoteCard key={note.id} note={note} />
+          );
         })}
       </div>
     </div>
--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -46,7 +46,7 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
 
       <Dialog.Portal>
         <Dialog.Overlay className="inset-0 fixed bg-black/50" />
-        <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
+        <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[500px] w-full h-[45vh] bg-slate-700 rounded-md flex flex-col outline-none">
           <Dialog.Close className="absolute hover:bg-red-500 transition-colors duration-300 ease-in-out right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
             <X className="size-5" />
           </Dialog.Close>
