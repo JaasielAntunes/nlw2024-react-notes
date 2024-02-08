36fa2307bce9d0ac3b6ae24b0b34a09228675edb
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-02-08 16:15:13 -0300

    Responsividade

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -98,7 +98,7 @@ export function NewNoteCard({ noteCreated }: NewNoteCardProps) {
 
       <Dialog.Portal>
         <Dialog.Overlay className="inset-0 fixed bg-black/50" />
-        <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[500px] w-full h-[45vh] bg-slate-700 rounded-md flex flex-col outline-none">
+        <Dialog.Content className="fixed overflow-hidden inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[500px] w-full md:h-[45vh] bg-slate-700 md:rounded-md flex flex-col outline-none">
           <Dialog.Close className="absolute hover:bg-red-500 transition-colors duration-300 ease-in-out right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
             <X className="size-5" />
           </Dialog.Close>
